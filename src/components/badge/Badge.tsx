eb2dd9677a9f11916e57f20f04667b2085d5abcd
Alfio Martini <alfio.martini@encora.com>
2024-02-13 21:00:12 -0300

    add styled components to Badge

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,8 +1,8 @@
 import { Dispatch } from "react";
 import { CiEdit } from "react-icons/ci";
 import { RiDeleteBinLine } from "react-icons/ri";
-import "./styles.css";
 import { Ingredient } from "../../interfaces";
+import styled from "styled-components";
 
 interface Props {
   name: string;
@@ -22,20 +22,40 @@ export function Badge({
   setCurrentIngredient,
 }: Props) {
   return (
-    <span className="badge">
+    <BadgeContainer>
       {name} ({quantity}){" "}
-      <span className="badge-icons">
+      <BadgeIcons>
         <CiEdit
+          size="1.3em"
           style={{ cursor: "pointer" }}
           onClick={() =>
             setCurrentIngredient({ name, id, quantity, description })
           }
         />{" "}
         <RiDeleteBinLine
+          size="1.3em"
           style={{ cursor: "pointer" }}
           onClick={() => removeIngredient(id)}
         />
-      </span>
-    </span>
+      </BadgeIcons>
+    </BadgeContainer>
   );
 }
+
+const BadgeContainer = styled.span`
+  padding: 5px 10px;
+  border-radius: 8px;
+  background-color: lightsteelblue;
+  color: black;
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+`;
+
+const BadgeIcons = styled.span`
+  display: flex;
+  gap: 10px;
+`;
