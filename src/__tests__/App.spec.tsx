692973bf004475d2e4d3fbada652f97e7c356d5b
Alfio Martini <alfio.martini@encora.com>
2024-02-15 16:00:45 -0300

    add tests to App

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,17 +1,58 @@
-// import { render } from "@testing-library/react";
-// import App from "../App";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import App from "../App";
 
-// jest.mock("react-router-dom", () => {
-//   // Require the original module to not be mocked...
-//   const originalModule = jest.requireActual("react-router-dom");
+jest.mock("../pages/Home", () => ({
+  __esModule: true,
+  Home: () => <div>Mock Landing Page</div>,
+}));
 
-//   return {
-//     __esModule: true,
-//     ...originalModule,
-//     // add your noops here
-//   };
-// });
+jest.mock("../components/burgers/Burgers", () => ({
+  __esModule: true,
+  Burgers: () => <div>Mock Component Burgers</div>,
+}));
 
-test("demo", () => {
-  expect(true).toBeTruthy();
+jest.mock("../components/orders/Orders", () => ({
+  __esModule: true,
+  Orders: () => <div>Mock Component Orders</div>,
+}));
+
+jest.mock("../components/ingredients/Ingredients", () => ({
+  __esModule: true,
+  Ingredients: () => <div>Mock Component Ingredients</div>,
+}));
+
+describe("<App />", () => {
+  it("should render the home page", () => {
+    render(<App />, { wrapper: BrowserRouter });
+    expect(screen.getByText(/mock landing page/i)).toBeInTheDocument();
+  });
+
+  it("should render the Burgers Page", () => {
+    render(
+      <MemoryRouter initialEntries={["/burgers"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/mock component burgers/i)).toBeInTheDocument();
+  });
+
+  it("should render the Orders Page", () => {
+    render(
+      <MemoryRouter initialEntries={["/orders"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/mock component orders/i)).toBeInTheDocument();
+  });
+
+  it("should render the Ingredients Page", () => {
+    render(
+      <MemoryRouter initialEntries={["/ingredients"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/mock component ingredients/i)).toBeInTheDocument();
+  });
 });
