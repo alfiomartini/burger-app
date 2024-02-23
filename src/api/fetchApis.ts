a9126e1e7478bc345235e04c9ba70bc55e9aa4bc
Alfio Martini <alfio.martini@encora.com>
2024-02-23 05:39:58 -0300

    fix ingredient names's endpoints

--- src/api/fetchApis.ts
+++ src/api/fetchApis.ts
@@ -2,28 +2,28 @@ import { client } from "./httpClient";
 import { Ingredient, Burger, WeakIngredient } from "../interfaces";
 
 export async function getIngredients(): Promise<Ingredient[]> {
-  const response = await client.get("/ingredients");
+  const response = await client.get("/ingredient");
   return response.data as Ingredient[];
 }
 
 export async function getBurgers(): Promise<Burger[]> {
-  const response = await client.get("/burgers");
+  const response = await client.get("/burger");
   return response.data as Burger[];
 }
 
 export async function createIngredient(
   item: WeakIngredient
 ): Promise<Ingredient> {
-  const response = await client.post("/ingredients", item);
+  const response = await client.post("/ingredient", item);
   return response.data as Ingredient;
 }
 
 export async function deleteIngredient(id: string): Promise<Ingredient> {
-  const response = await client.delete(`/ingredients/${id}`);
+  const response = await client.delete(`/ingredient/${id}`);
   return response.data as Ingredient;
 }
 
 export async function updateIngredient(item: Ingredient): Promise<Ingredient> {
-  const response = await client.patch(`/ingredients/${item.id}`, item);
+  const response = await client.patch(`/ingredient/${item.id}`, item);
   return response.data as Ingredient;
 }
